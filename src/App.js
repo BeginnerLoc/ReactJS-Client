e9d0ec8f951800767950df660d3115f942d0057b
tienloctran <loctientran235@gmail.com>
2023-05-06 16:25:54 +0800

    create routing mechanism to the 3 pages

--- src/App.js
+++ src/App.js
@@ -1,92 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import socketIOClient from 'socket.io-client';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Streaming from './pages/Streaming';
 
-const ENDPOINT = "http://localhost:5000";
 
-function VideoStream() {
-  const [socket, setSocket] = useState(null);
-  const [stream, setStream] = useState(null);
-  const [videoData, setVideoData] = useState(null);
-
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    setSocket(socket);
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!stream) {
-      return;
-    }
-
-    const interval = setInterval(() => {
-      const canvas = document.createElement('canvas');
-      canvas.width = stream.getVideoTracks()[0].getSettings().width;
-      canvas.height = stream.getVideoTracks()[0].getSettings().height;
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      const dataUrl = canvas.toDataURL('image/jpeg');
-      const base64Data = dataUrl.replace(/^data:image\/(png|jpeg|jpg);base64,/, "");
-      setVideoData(base64Data);
-    }, 1000 / 30); // Send 30 frames per second
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [stream]);
-
-  const videoRef = React.useRef(null);
-
-  const startVideoStream = async () => {
-    const constraints = {
-      audio: false,
-      video: true
-    };
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      setStream(stream);
-      videoRef.current.srcObject = stream;
-      videoRef.current.play();
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const stopVideoStream = () => {
-    if (stream) {
-      stream.getTracks().forEach(track => {
-        track.stop();
-      });
-      setStream(null);
-      setVideoData(null);
-    }
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (socket && videoData) {
-        socket.emit('sendStream', { image: videoData });
-      }
-    }, 1000 / 30); // Send 30 frames per second
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [socket, videoData]);
-
+function App() {
   return (
-    <div>
-      <video ref={videoRef} />
-      <div>
-        <button onClick={startVideoStream}>Start Video Stream</button>
-        <button onClick={stopVideoStream}>Stop Video Stream</button>
-      </div>
-    </div>
-  );
-}
+    <div className="app">
+      <nav>
+        <ul>
+          {/* if used <a> instead of <link>, the whole browser would reload each time the routing is triggered */}
+          {/* CANNOT use <link> to redirect to external path */}
+          <li>
+            <Link to="/Home">Home</Link>
+          </li>
+          <li>
+            <Link to="/Dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/Streaming">Streaming</Link>
+          </li>
+        </ul>
+      </nav>
 
-export default VideoStream;
+      {/* each time the <link> is clicked, react-router will used the address on the browser's address bar to check against matching path in route */}
+      <Routes>
+        <Route path='/Home' element={<Home/>}></Route>
+        <Route path='/Dashboard' element={<Dashboard/>}></Route>
+        <Route path='/Streaming' element={<Streaming/>}></Route>
+      </Routes>
+    </div>
+  )
+}
+export default App
