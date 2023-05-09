242ce105840081ba148d0364212ea22411e842b9
tienloctran <loctientran235@gmail.com>
2023-05-09 19:04:16 +0800

    complete theme setup

--- src/App.js
+++ src/App.js
@@ -3,34 +3,43 @@ import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Streaming from './pages/Streaming';
 import { ColorModeContext, useMode } from './theme';
-import { CssBaseLine, ThemeProvider} from '@mui/material'
+import { CssBaseLine, ThemeProvider } from '@mui/material';
 
 function App() {
-  return (
-    <div className="app">
-      <nav>
-        <ul>
-          {/* if used <a> instead of <link>, the whole browser would reload each time the routing is triggered */}
-          {/* CANNOT use <link> to redirect to external path */}
-          <li>
-            <Link to="/Home">Home</Link>
-          </li>
-          <li>
-            <Link to="/Dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/Streaming">Streaming</Link>
-          </li>
-        </ul>
-      </nav>
+  const [theme, colorMode] = useMode();
 
-      {/* each time the <link> is clicked, react-router will used the address on the browser's address bar to check against matching path in route */}
-      <Routes>
-        <Route path='/Home' element={<Home/>}></Route>
-        <Route path='/Dashboard' element={<Dashboard/>}></Route>
-        <Route path='/Streaming' element={<Streaming/>}></Route>
-      </Routes>
-    </div>
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseLine />
+        <div className="app">
+          <main className='content'></main>
+          <nav>
+            <ul>
+              {/* if used <a> instead of <link>, the whole browser would reload each time the routing is triggered */}
+              {/* CANNOT use <link> to redirect to external path */}
+              <li>
+                <Link to="/Home">Home</Link>
+              </li>
+              <li>
+                <Link to="/Dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/Streaming">Streaming</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* each time the <link> is clicked, react-router will used the address on the browser's address bar to check against matching path in route */}
+          <Routes>
+            <Route path='/Home' element={<Home />}></Route>
+            <Route path='/Dashboard' element={<Dashboard />}></Route>
+            <Route path='/Streaming' element={<Streaming />}></Route>
+          </Routes>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 export default App
