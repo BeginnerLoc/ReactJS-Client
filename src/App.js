bad152260d7cfb1e1f2961642ee4f7d34d9cb2df
tienloctran <loctientran235@gmail.com>
2023-05-09 19:59:10 +0800

    use theme in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dashboard from './pages/Dashboard';
 import Streaming from './pages/Streaming';
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseLine, ThemeProvider } from '@mui/material';
+import Topbar from './scenes/global/Topbar';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -14,29 +15,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseLine />
         <div className="app">
-          <main className='content'></main>
-          <nav>
-            <ul>
-              {/* if used <a> instead of <link>, the whole browser would reload each time the routing is triggered */}
-              {/* CANNOT use <link> to redirect to external path */}
-              <li>
-                <Link to="/Home">Home</Link>
-              </li>
-              <li>
-                <Link to="/Dashboard">Dashboard</Link>
-              </li>
-              <li>
-                <Link to="/Streaming">Streaming</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* each time the <link> is clicked, react-router will used the address on the browser's address bar to check against matching path in route */}
-          <Routes>
-            <Route path='/Home' element={<Home />}></Route>
-            <Route path='/Dashboard' element={<Dashboard />}></Route>
-            <Route path='/Streaming' element={<Streaming />}></Route>
-          </Routes>
+          <main className='content'>
+              <Topbar/>
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
