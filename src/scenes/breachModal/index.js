3c1fb3fd94fe418a016054fa0a339a2618038f35
IT Helpdesk <131857300+ChristopherChng@users.noreply.github.com>
2023-08-12 01:27:19 +0800

    Included the case info into the modal

--- src/scenes/breachModal/index.js
+++ src/scenes/breachModal/index.js
@@ -6,10 +6,13 @@ const BreachModal = ({ closeModal, isModalOpen, selectedBreachId }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const [imageData, setImageData] = useState(""); // State to hold the base64 image data
-  const [workerNameData, setworkerNameData] = useState(""); // State to hold the base64 image data
-  const [descriptionData, setdescriptionData] = useState(""); // State to hold the base64 image data
-  const [breachIDData, setbreachIDData] = useState(""); // State to hold the base64 image data
-  const [locationData, setlocationData] = useState(""); // State to hold the base64 image data
+  const [workerNameData, setworkerNameData] = useState(""); // State to hold the worker name data
+  const [descriptionData, setdescriptionData] = useState(""); // State to hold the description data
+  // const [breachIDData, setbreachIDData] = useState(""); // State to hold the breach ID data
+  const [locationData, setlocationData] = useState(""); // State to hold the location data
+  const [caseResolvedData, setcaseResolvedData] = useState(""); // State to hold the case resolved data
+  const [caseResolvedTimeData, setcaseResolvedTimeData] = useState(""); // State to hold the case resolved time data
+  const [caseResolutionData, setcaseResolutionData] = useState(""); // State to hold the case resolution data
   const URL = 'http://localhost:5000';
 
   useEffect(() => {
@@ -28,8 +31,11 @@ const BreachModal = ({ closeModal, isModalOpen, selectedBreachId }) => {
         setImageData(data.image);
         setworkerNameData(data.worker_name);
         setdescriptionData(data.description);
-        setbreachIDData(data.breach_id);
+        // setbreachIDData(data.breach_id); // Not in used, but can be enabled in the future
         setlocationData(data.location);
+        setcaseResolvedData(data.case_resolved);
+        setcaseResolvedTimeData(data.case_resolved_time);
+        setcaseResolutionData(data.case_resolution);
 
       })
       .catch((error) => {
@@ -75,6 +81,9 @@ const BreachModal = ({ closeModal, isModalOpen, selectedBreachId }) => {
         <p id="modal-worker_name"><b>Name: </b>{workerNameData}</p>
         <p id="modal-description"><b>Breach Type: </b> {descriptionData}</p>
         <p id="modal-location"><b>Location: </b> {locationData}</p>
+        <p id="modal-location"><b>Case Resolved: </b> {caseResolvedData}</p>
+        <p id="modal-location"><b>Case Resolved Time: </b> {caseResolvedTimeData}</p>
+        <p id="modal-location"><b>Case Resolution: </b> {caseResolutionData}</p>
 
         <Button variant="contained" onClick={closeModal}>
           Close
